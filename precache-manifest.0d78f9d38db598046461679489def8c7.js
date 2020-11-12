48d507e7bd9e04a23fc1620bd56b607dca7aef20
Jacek Jarczok <kson.eu@gmail.com>
2020-11-12 13:30:53 +0100

    Updates

---
+++ precache-manifest.0d78f9d38db598046461679489def8c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b859d768c499d86fb52b",
+    "url": "/react-lights-out-game/static/css/main.612a6783.chunk.css"
+  },
+  {
+    "revision": "b859d768c499d86fb52b",
+    "url": "/react-lights-out-game/static/js/main.b859d768.chunk.js"
+  },
+  {
+    "revision": "a989bfac1d920b4f2190",
+    "url": "/react-lights-out-game/static/js/1.a989bfac.chunk.js"
+  },
+  {
+    "revision": "cca055eebb7f62a6ae6f",
+    "url": "/react-lights-out-game/static/js/runtime~main.cca055ee.js"
+  },
+  {
+    "revision": "c769182d87590aff4e952e21cd341935",
+    "url": "/react-lights-out-game/index.html"
+  }
+];
